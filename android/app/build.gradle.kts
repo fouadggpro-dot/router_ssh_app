0e7fdfb23675b3cd0782cf2891a5aedcb5b5d8f6
fouadggpro.dot <fouadggpro@gmail.com>
2026-08-30 11:42:26 +0300

    Fix build.gradle.kts syntax to match Kotlin DSL

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,48 +1,48 @@
 plugins {
-    id "com.android.application"
-    id "kotlin-android"
-    id "dev.flutter.flutter-gradle-plugin"
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace "com.example.router_controller"
-    compileSdk 34
+    namespace = "com.example.router_controller"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId "com.example.router_controller"
-        minSdk 21
-        targetSdk 34
-        versionCode 1
-        versionName "1.0.0"
+        applicationId = "com.example.router_controller"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0.0"
 
         ndk {
-            abiFilters 'armeabi-v7a', 'arm64-v8a', 'x86_64'
+            abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a", "x86_64"))
         }
     }
 
     buildTypes {
-        release {
-            signingConfig signingConfigs.debug
-            minifyEnabled false
-            shrinkResources false
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_17
-        targetCompatibility JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = '17'
+        jvmTarget = "17"
     }
 }
 
 flutter {
-    source '../..'
+    source = "../.."
 }
 
 dependencies {
-    implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk8"
-    implementation "com.jcraft:jsch:0.1.55"
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.jcraft:jsch:0.1.55")
 }
